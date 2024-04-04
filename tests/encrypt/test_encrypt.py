7dfbe381e3edb4f25e8d024f65036506c62a92ae
mairess <trabalho.maires@gmail.com>
2024-04-04 08:55:19 -0300

    test: test_encrypt_message implemented

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError) as error:
+        encrypt_message("Hello world!", "little potato")
+    assert str(error.value) == "tipo inválido para key"
+
+    with pytest.raises(TypeError) as error:
+        encrypt_message(2024, 2)
+    assert str(error.value) == "tipo inválido para message"
+
+    assert encrypt_message("XFAIL", 99) == "LIAFX"
+
+    assert encrypt_message("XFAIL", 3) == "AFX_LI"
+
+    assert encrypt_message("XFAIL", 4) == "L_IAFX"
