29fd2bd436b18a178de05c97cfaa99f0fe828b3c
mairess <trabalho.maires@gmail.com>
2024-04-04 17:50:01 -0300

    feat: implements is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,10 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    size = len(word)
+
+    for index in range(0, size // 2):
+        if word[index] != word[size - index - 1]:
+            return False
+    return True
