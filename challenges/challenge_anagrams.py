3cac3f094f6e772513e84ee8c9a454f67380136d
mairess <trabalho.maires@gmail.com>
2024-04-04 15:57:05 -0300

    feat: implements is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,36 @@
+def quick_sort(string, start, end):
+    if start < end:
+        p = partition(string, start, end)
+        quick_sort(string, start, p - 1)
+        quick_sort(string, p + 1, end)
+
+
+def partition(entry, start, end):
+    pivot = entry[end]
+    delimiter = start - 1
+
+    for index in range(start, end):
+        if entry[index] <= pivot:
+            delimiter = delimiter + 1
+            entry[index], entry[delimiter] = entry[delimiter], entry[index]
+
+    entry[delimiter + 1], entry[end] = entry[end], entry[delimiter + 1]
+
+    return delimiter + 1
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not first_string and not second_string:
+        return ("", "", False)
+
+    string_one = list(first_string.lower())
+    string_two = list(second_string.lower())
+
+    quick_sort(string_one, 0, len(string_one) - 1)
+    quick_sort(string_two, 0, len(string_two) - 1)
+
+    return (
+        "".join(string_one),
+        "".join(string_two),
+        "".join(string_one) == "".join(string_two),
+    )
