ad9c6a52b310554036937e42f61495d6aa226fd8
mairess <trabalho.maires@gmail.com>
2024-04-04 15:46:48 -0300

    Revert "refactor: study_schedule add decorator"
    This reverts commit 488ae4a2c84302eb27c075e4ef4a831a424e9803.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,20 @@
-from decorators.validate_permanence_period import is_valid_period
+def is_valid_period(permanence_period: list[tuple[int, int]]) -> bool:
+    for period in permanence_period:
+        if not (
+            isinstance(period, tuple)
+            and len(period) == 2
+            and all(isinstance(item, int) for item in period)
+        ):
+            return False
+    return True
 
 
-@is_valid_period
 def study_schedule(permanence_period: list[tuple[int, int]], target_time: int):
+    if not is_valid_period(permanence_period) or not isinstance(
+        target_time, int
+    ):
+        return None
+
     present_students = 0
     for period in permanence_period:
         if target_time >= period[0] and target_time <= period[1]:
