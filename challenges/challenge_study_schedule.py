c8e6d9e4e5031a676c7d64b01d0186942bce2b38
mairess <trabalho.maires@gmail.com>
2024-04-04 17:48:16 -0300

    perf(study_schedule): optimize code for large permanence_period arrays

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,23 +1,13 @@
-def is_valid_period(permanence_period: list[tuple[int, int]]) -> bool:
-    for period in permanence_period:
-        if not (
-            isinstance(period, tuple)
-            and len(period) == 2
-            and all(isinstance(item, int) for item in period)
-        ):
-            return False
-    return True
-
-
 def study_schedule(permanence_period: list[tuple[int, int]], target_time: int):
-    if not is_valid_period(permanence_period) or not isinstance(
-        target_time, int
-    ):
+    if not permanence_period or target_time is None:
         return None
 
-    present_students = 0
-    for period in permanence_period:
-        if target_time >= period[0] and target_time <= period[1]:
-            present_students += 1
+    for start, end in permanence_period:
+        if (
+            not isinstance(start, int)
+            or not isinstance(end, int)
+            or start > end
+        ):
+            return None
 
-    return present_students
+    return sum(start <= target_time <= end for start, end in permanence_period)
