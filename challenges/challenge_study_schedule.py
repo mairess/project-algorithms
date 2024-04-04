7a52e2a45e2eef8595ae897ae3bfafe66eca8225
mairess <trabalho.maires@gmail.com>
2024-04-04 15:24:31 -0300

    refactor: study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,20 +1,23 @@
-def is_valid_entry(entry: tuple[int, int]) -> bool:
-    return (
-        isinstance(entry, tuple)
-        and len(entry) == 2
-        and all(isinstance(item, int) for item in entry)
-    )
+def is_valid_period(permanence_period: list[tuple[int, int]]) -> bool:
+    for period in permanence_period:
+        if not (
+            isinstance(period, tuple)
+            and len(period) == 2
+            and all(isinstance(item, int) for item in period)
+        ):
+            return False
+    return True
 
 
-def study_schedule(permanence_period, target_time):
-    if any(
-        not is_valid_entry(entry) for entry in permanence_period
-    ) or not isinstance(target_time, int):
+def study_schedule(permanence_period: list[tuple[int, int]], target_time: int):
+    if not is_valid_period(permanence_period) or not isinstance(
+        target_time, int
+    ):
         return None
 
     present_students = 0
-    for student_time in permanence_period:
-        if target_time >= student_time[0] and target_time <= student_time[1]:
+    for period in permanence_period:
+        if target_time >= period[0] and target_time <= period[1]:
             present_students += 1
 
     return present_students
